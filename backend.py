cd8f661e963d98582587bb0c3372c9a4ba00bc3c
Yeseviola <aryesigon@gmail.com>
2025-01-22 23:52:20 -0300

    resolviendo otros detalles del local

--- backend.py
+++ backend.py
@@ -167,6 +167,6 @@ def process_document():
         return jsonify({'error': 'Tipo de archivo no soportado'}), 400
 
 if __name__ == '__main__':
-     port = int(os.environ.get("PORT", 10000))
+     port = int(os.environ.get("PORT", 5000))
      app.run(host="0.0.0.0", port=port, debug=True)
 
